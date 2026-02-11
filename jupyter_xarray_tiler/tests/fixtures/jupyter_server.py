8fcf47a9d921d98e818bd7f3378f743998b04c99
Matt Fisher <mfisher87@gmail.com>
2026-02-11 16:51:08 -0700

    Add type annotation to fixture

--- jupyter_xarray_tiler/tests/fixtures/jupyter_server.py
+++ jupyter_xarray_tiler/tests/fixtures/jupyter_server.py
@@ -1,8 +1,10 @@
+from typing import Any
+
 import pytest
 
 
 @pytest.fixture
-def jp_server_config(jp_server_config) -> dict:
+def jp_server_config(jp_server_config: Any) -> dict[str, Any]:  # noqa: ANN401
     return {
         "ServerApp": {
             "jpserver_extensions": {
