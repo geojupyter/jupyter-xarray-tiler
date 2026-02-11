bd0833ead03cf2f4d7010f2f54a150da8c143fa2
Matt Fisher <mfisher87@gmail.com>
2026-02-11 16:32:33 -0700

    Dynamically discover fixture "plugins"

--- conftest.py
+++ conftest.py
@@ -1,9 +1,19 @@
+from pathlib import Path
+
 import pytest
+
+# Discover all modules in the tests.fixtures subpackage
+FIXTURES_MODULE = ["jupyter_xarray_tiler", "tests", "fixtures"]
+fixtures_dir = Path(__file__).parent.joinpath(*FIXTURES_MODULE)
+fixture_plugins = [
+    f"{'.'.join(FIXTURES_MODULE)}.{path.stem}"
+    for path in fixtures_dir.glob("*.py")
+    if not path.stem.startswith("_")
+]
 
 pytest_plugins = (
     "pytest_jupyter.jupyter_server",
-    "jupyter_xarray_tiler.tests.fixtures.server",
-    "jupyter_xarray_tiler.tests.fixtures.data",
+    *fixture_plugins,
 )
 
 
