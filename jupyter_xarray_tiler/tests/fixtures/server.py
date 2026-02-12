4ae90f07d9c1ef9653ef512278e552acbae4d272
Matt Fisher <mfisher87@gmail.com>
2026-02-11 17:26:23 -0700

    More consistent fixture name

--- jupyter_xarray_tiler/tests/fixtures/server.py
+++ jupyter_xarray_tiler/tests/fixtures/server.py
@@ -6,7 +6,7 @@ from jupyter_xarray_tiler.titiler._server import TiTilerServer
 
 
 @pytest_asyncio.fixture
-async def titiler_server() -> AsyncGenerator[TiTilerServer]:
+async def clean_titiler_server() -> AsyncGenerator[TiTilerServer]:
     server = TiTilerServer()
     await server.start_tile_server()
     yield server
--- jupyter_xarray_tiler/tests/test_titiler_server.py
+++ jupyter_xarray_tiler/tests/test_titiler_server.py
@@ -18,34 +18,34 @@ async def test_server_is_not_singleton() -> None:
 
 @pytest.mark.asyncio
 async def test_add_data_array_creates_api_routes(
-    titiler_server: TiTilerServer,
+    clean_titiler_server: TiTilerServer,
     random_data_array: DataArray,
 ) -> None:
     """Test that FastAPI routes are created when a data array is added to the server."""
-    assert len(titiler_server.routes) == 0
+    assert len(clean_titiler_server.routes) == 0
 
-    await titiler_server.add_data_array(
+    await clean_titiler_server.add_data_array(
         data_array=random_data_array,
         colormap_name="viridis",
     )
 
-    assert len(titiler_server.routes) > 1
+    assert len(clean_titiler_server.routes) > 1
 
 
 @pytest.mark.asyncio
 async def test_add_data_array_returns_valid_tile_url(
-    titiler_server: TiTilerServer,
+    clean_titiler_server: TiTilerServer,
     random_data_array: DataArray,
 ) -> None:
     """Test that adding a DataArray returns a properly formatted tile URL."""
-    tile_url = await titiler_server.add_data_array(
+    tile_url = await clean_titiler_server.add_data_array(
         data_array=random_data_array,
         colormap_name="viridis",
     )
 
     assert tile_url is not None
     assert "/proxy/" in tile_url
-    assert f"/{titiler_server._port}/" in tile_url
+    assert f"/{clean_titiler_server._port}/" in tile_url
     assert "/tiles/WebMercatorQuad/{z}/{x}/{y}.png" in tile_url
     assert "colormap_name=viridis" in tile_url
     assert "scale=1" in tile_url
