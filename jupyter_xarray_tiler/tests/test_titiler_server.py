b864496a8b91ee525a16101585399e162f5b7993
Matt Fisher <mfisher87@gmail.com>
2026-04-30 15:17:17 -0600

    Move some unit tests into a class

--- jupyter_xarray_tiler/tests/test_titiler_server.py
+++ jupyter_xarray_tiler/tests/test_titiler_server.py
@@ -7,52 +7,55 @@ from .helpers import check_tile
 from .params import params_for_backend
 
 
-@pytest.mark.asyncio
-async def test_server_is_not_singleton() -> None:
-    """Test that TiTilerServer is not a singleton.
+class TestTiTilerServer:
+    @pytest.mark.asyncio
+    async def test_server_is_not_singleton(self) -> None:
+        """Test that TiTilerServer is not a singleton.
 
-    Previously, we used a singleton pattern for TiTiler server, but not anymore.
-    Now, tests depend on being able to create a fresh instance and the end-user is
-    protected from starting multiple instances in the public API.
-    """
-    assert id(TiTilerServer()) != id(TiTilerServer())
-    assert TiTilerServer() is not TiTilerServer()
+        Previously, we used a singleton pattern for TiTiler server, but not anymore.
+        Now, tests depend on being able to create a fresh instance and the end-user is
+        protected from starting multiple instances in the public API.
+        """
+        assert id(TiTilerServer()) != id(TiTilerServer())
+        assert TiTilerServer() is not TiTilerServer()
 
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        ("z", "y", "x", "mock_data_array"),
+        params_for_backend("titiler"),
+        indirect=["mock_data_array"],
+    )
+    async def test_add_data_array_works(
+        self,
+        z: int,
+        y: int,
+        x: int,
+        clean_titiler_server: TiTilerServer,
+        mock_data_array: DataArray,
+    ) -> None:
+        """Test that FastAPI routes are created when a data array is added to server."""
+        assert len(clean_titiler_server.routes) == 0
 
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    ("z", "y", "x", "mock_data_array"),
-    params_for_backend("titiler"),
-    indirect=["mock_data_array"],
-)
-async def test_add_data_array_works(
-    z: int,
-    y: int,
-    x: int,
-    clean_titiler_server: TiTilerServer,
-    mock_data_array: DataArray,
-) -> None:
-    """Test that FastAPI routes are created when a data array is added to the server."""
-    assert len(clean_titiler_server.routes) == 0
+        proxy_url = await clean_titiler_server.add_data_array(
+            data_array=mock_data_array
+        )
 
-    proxy_url = await clean_titiler_server.add_data_array(data_array=mock_data_array)
+        assert len(clean_titiler_server.routes) > 0
 
-    assert len(clean_titiler_server.routes) > 0
+        await check_tile(proxy_url=proxy_url.format(z=z, y=y, x=x))
 
-    await check_tile(proxy_url=proxy_url.format(z=z, y=y, x=x))
+    @pytest.mark.asyncio
+    async def test_add_data_array_returns_valid_tile_url(
+        self,
+        clean_titiler_server: TiTilerServer,
+        mock_data_array: DataArray,
+    ) -> None:
+        """Test that adding a DataArray returns a properly formatted tile URL."""
+        tile_url = await clean_titiler_server.add_data_array(data_array=mock_data_array)
 
-
-@pytest.mark.asyncio
-async def test_add_data_array_returns_valid_tile_url(
-    clean_titiler_server: TiTilerServer,
-    mock_data_array: DataArray,
-) -> None:
-    """Test that adding a DataArray returns a properly formatted tile URL."""
-    tile_url = await clean_titiler_server.add_data_array(data_array=mock_data_array)
-
-    assert tile_url is not None
-    assert "/proxy/" in tile_url
-    assert f"/{clean_titiler_server._port}/" in tile_url
-    assert "/tiles/WebMercatorQuad/{z}/{x}/{y}.png" in tile_url
-    assert "colormap_name=viridis" in tile_url
-    assert "scale=1" in tile_url
+        assert tile_url is not None
+        assert "/proxy/" in tile_url
+        assert f"/{clean_titiler_server._port}/" in tile_url
+        assert "/tiles/WebMercatorQuad/{z}/{x}/{y}.png" in tile_url
+        assert "colormap_name=viridis" in tile_url
+        assert "scale=1" in tile_url
--- jupyter_xarray_tiler/tests/test_xpublish_server.py
+++ jupyter_xarray_tiler/tests/test_xpublish_server.py
@@ -7,51 +7,52 @@ from .helpers import check_tile
 from .params import params_for_backend
 
 
-@pytest.mark.asyncio
-async def test_server_is_not_singleton() -> None:
-    """Test that XpublishServer is not a singleton.
+class TestXpublishServer:
+    @pytest.mark.asyncio
+    async def test_server_is_not_singleton(self) -> None:
+        """Test that XpublishServer is not a singleton.
 
-    Previously, we used a singleton pattern for Xpublish server, but not anymore.
-    Now, tests depend on being able to create a fresh instance and the end-user is
-    protected from starting multiple instances in the public API.
-    """
-    assert id(XpublishServer()) != id(XpublishServer())
-    assert XpublishServer() is not XpublishServer()
+        Previously, we used a singleton pattern for Xpublish server, but not anymore.
+        Now, tests depend on being able to create a fresh instance and the end-user is
+        protected from starting multiple instances in the public API.
+        """
+        assert id(XpublishServer()) != id(XpublishServer())
+        assert XpublishServer() is not XpublishServer()
 
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    ("z", "y", "x", "mock_data_array"),
-    params_for_backend("xpublish"),
-    indirect=["mock_data_array"],
-)
-async def test_add_data_array_works(
-    z: int,
-    y: int,
-    x: int,
-    clean_xpublish_server: XpublishServer,
-    mock_data_array: DataArray,
-) -> None:
-    """Test that tiles can be accessed after a data array is added to the server."""
-    proxy_url = await clean_xpublish_server.add_data_array(
-        data_array=mock_data_array,
-        rescale=(0, 1),
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        ("z", "y", "x", "mock_data_array"),
+        params_for_backend("xpublish"),
+        indirect=["mock_data_array"],
     )
+    async def test_add_data_array_works(
+        self,
+        z: int,
+        y: int,
+        x: int,
+        clean_xpublish_server: XpublishServer,
+        mock_data_array: DataArray,
+    ) -> None:
+        """Test that tiles can be accessed after a data array is added to the server."""
+        proxy_url = await clean_xpublish_server.add_data_array(
+            data_array=mock_data_array,
+            rescale=(0, 1),
+        )
 
-    await check_tile(proxy_url=proxy_url.format(z=z, y=y, x=x))
+        await check_tile(proxy_url=proxy_url.format(z=z, y=y, x=x))
 
+    @pytest.mark.asyncio
+    async def test_add_data_array_returns_valid_tile_url(
+        self,
+        clean_xpublish_server: XpublishServer,
+        mock_data_array: DataArray,
+    ) -> None:
+        """Test that adding a DataArray returns a properly formatted tile URL."""
+        tile_url = await clean_xpublish_server.add_data_array(
+            data_array=mock_data_array,
+        )
 
-@pytest.mark.asyncio
-async def test_add_data_array_returns_valid_tile_url(
-    clean_xpublish_server: XpublishServer,
-    mock_data_array: DataArray,
-) -> None:
-    """Test that adding a DataArray returns a properly formatted tile URL."""
-    tile_url = await clean_xpublish_server.add_data_array(
-        data_array=mock_data_array,
-    )
-
-    assert tile_url is not None
-    assert "/proxy/" in tile_url
-    assert f"/{clean_xpublish_server._port}/" in tile_url
-    assert "/tiles/WebMercatorQuad/{z}/{y}/{x}" in tile_url
+        assert tile_url is not None
+        assert "/proxy/" in tile_url
+        assert f"/{clean_xpublish_server._port}/" in tile_url
+        assert "/tiles/WebMercatorQuad/{z}/{y}/{x}" in tile_url
